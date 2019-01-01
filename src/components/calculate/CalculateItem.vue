59daa4aa076a11fe496d9eb19e2d9dc949e7502d
kcopper8 <kcopper8@gmail.com>
2019-01-01 14:05:49 +0900

    add formatting

--- src/components/calculate/CalculateItem.vue
+++ src/components/calculate/CalculateItem.vue
@@ -2,8 +2,7 @@
   <v-list-group :value="true">
     <v-list-tile slot="activator">
       <v-list-tile-content>
-        <v-list-tile-title>{{member.name}}</v-list-tile-title>
-        <v-list-tile-sub-title>￦{{fullCharge}}</v-list-tile-sub-title>
+        <v-list-tile-title>{{member.name}} : {{fullCharge}}</v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile>
     <v-list-tile
@@ -13,10 +12,10 @@
     >
       <v-list-tile-content>
         <v-list-tile-title>
-          <span class="subheading">[{{goods.name}}]</span> 개인 부담: ￦{{goods.unitPrice * goods.count / goods.charges.length}}
+          <span class="subheading">[{{goods.name}}]</span> 개인 부담: {{getIndividualCharge(goods.unitPrice , goods.count , goods.charges.length)}}
         </v-list-tile-title>
         <v-list-tile-sub-title>
-          메뉴 전체: ￦{{goods.unitPrice * goods.count}}
+          메뉴 전체: {{getFullMenuPrice(goods.unitPrice, goods.count)}}
         </v-list-tile-sub-title>
       </v-list-tile-content>
     </v-list-tile>
@@ -25,15 +24,29 @@
 </template>
 
 <script>
+function formatCurrency(number) {
+  return new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: 'KRW'
+  }).format(Math.round(number))
+}
 export default {
   props: ['member', 'calculateItem'],
   computed: {
     fullCharge() {
-      return this.calculateItem.reduce((chargeSum, goods) => {
+      return formatCurrency(this.calculateItem.reduce((chargeSum, goods) => {
         const currentCharge = goods.unitPrice * goods.count / goods.charges.length;
 
         return chargeSum + currentCharge;
-      }, 0);
+      }, 0));
+    }
+  },
+  methods: {
+    getIndividualCharge(unitPrice, count, chargesCount) {
+      return formatCurrency(unitPrice * count / chargesCount);
+    },
+    getFullMenuPrice(unitPrice, goodsCound) {
+      return formatCurrency(unitPrice * goodsCound)
     }
   }
 }
